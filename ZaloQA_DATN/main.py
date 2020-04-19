ff6686454ed1a614b4e822a7e02afa370145d749
DatNgo <datngo.bk@gmail.com>
2020-04-20 00:06:48 +0700

    m

--- ZaloQA_DATN/main.py
+++ ZaloQA_DATN/main.py
@@ -21,16 +21,16 @@ config['lr'] = 4e-5
 
 
 zalo = ZaloDatasetProcessor()
-zalo.load_from_path(dataset_path='dataset', train_filename='train.json', test_filename='test.json')
+zalo.load_from_path(dataset_path='dataset', train_filename='train.json', test_filename='test.json', dev_filename=None, train_augmented_filename='train_bqa.json')
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
-features = zalo.convert_examples_to_features(zalo.train_data, zalo.label_list, 256, tokenizer)
-
+features_train = zalo.convert_examples_to_features(zalo.train_data, zalo.label_list, 256, tokenizer)
+features_test = zalo.convert_examples_to_features(zalo.test_data, zalo.label_list, 256, tokenizer)
 if __name__ == "__main__":
     
     NUM_OF_INTENT = 2
-    config_model = BertConfig.from_pretrained('bert-base-multilingual-cased', output_hidden_states=True)
+    config_model = BertConfig.from_pretrained('bert-base-multilingual-cased', output_hidden_states=True)#
 
 
     model = QAModel(config_model, NUM_OF_INTENT)
-    training = Training(features, model, logger, zalo.label_list, config)
+    training = Training(features_train, features_test, model, logger, zalo.label_list, config)
     training.train()
